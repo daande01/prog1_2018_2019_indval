e47afdd869fe0ca4849119e852bfa0357ebf26d6
daande01 <daniel.evert.andersson@gmail.com>
2018-12-10 15:47:12 +0100

    test

---
+++ prog1_2018_2019_indval/src/metoder/Metoder2.java
@@ -0,0 +1,41 @@
+package metoder;
+
+import javax.swing.JOptionPane;
+
+public class Metoder2 {
+
+	public static String getName() {
+
+		String name = JOptionPane.showInputDialog("ange namn ");
+
+		return name;
+
+	}
+
+	public static void gånger(int k) {
+
+		for (int i = 0; i <= 10; i++) {
+
+			int tal = k * i;
+			System.out.println(k+"*" + i + "=" + tal);
+		}
+			k=10000;
+	}
+
+	public static void main(String[] args) {
+
+		String tabell = JOptionPane.showInputDialog("ange vilken gånger tabell");
+		int tab= Integer.parseInt(tabell);
+
+		Metoder2.gånger(tab);
+
+	}
+
+
+
+
+
+
+
+
+}
---
+++ prog1_2018_2019_indval/src/metoder/Metoder3.java
@@ -0,0 +1,16 @@
+package metoder;
+
+public class Metoder3 {
+
+	public static void main(String[] args) {
+
+
+		String name =Metoder2.getName();
+
+		System.out.print(name);
+
+	}
+
+
+
+}
