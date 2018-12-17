43870b70292b2736ec3d317404d087c82385828b
daande01 <daniel.evert.andersson@gmail.com>
2018-12-17 15:53:37 +0100

    new konto

---
+++ prog1_2018_2019_indval/src/metoder/Konto.java
@@ -0,0 +1,55 @@
+package metoder;
+
+public class Konto {
+
+	private static double ränta =0.1;
+	private String name="";
+	private double saldo=0;
+
+	public Konto(String n,double s) {
+
+		System.out.println("Du har nu skapat ett konto");
+
+
+		name=n;
+		saldo=s;
+
+
+	}
+	public void årsRänta() {
+
+		saldo =saldo *(ränta+1);
+
+
+	}
+
+
+
+	public void ökaSaldo(double insättning) {
+
+		saldo=saldo+insättning;
+	}
+
+	public Double getSaldo() {
+
+		return saldo;
+	}
+
+
+	public static void main(String[] args) {
+
+
+		Konto k1=new Konto("daniel",43);
+		k1.ökaSaldo(100);
+
+		k1.årsRänta();
+
+		Konto k2=new Konto("evert",100);
+		k1.ökaSaldo(100);
+
+		System.out.println(k1.getSaldo());
+
+		System.out.println(k2.getSaldo());
+
+	}
+}
