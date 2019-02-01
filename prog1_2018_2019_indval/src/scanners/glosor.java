b7d8f1c5e14e7ed44bbebd8eceb5df57ac7f8389
daande01 <daniel.evert.andersson@gmail.com>
2019-02-01 10:58:02 +0100

    fler filer

--- prog1_2018_2019_indval/src/scanners/glosor.java
+++ prog1_2018_2019_indval/src/scanners/glosor.java
@@ -1,25 +1,48 @@
 package scanners;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class glosor {
+
+	static LinkedList<String> sv = new LinkedList<String>();
+	static LinkedList<String> eng = new LinkedList<String>();
 
 
 
 	public static void main(String[] args) {
 
 
-		Scanner s= new Scanner(System.in);
-
+		Scanner s = new Scanner(System.in);
 
 		System.out.println("1 nyglosa \n 2 test \n 3 visa lista");
 
+		int val = s.nextInt();
 
+		switch (val) {
+		case 1:
+			nyglosa();
 
+			break;
+		case 2:
+			// test();
+			break;
 
+		default:
+
+			break;
+		}
+
+	}
+
+	public static void nyglosa() {
+
+		sv.add("katt");
 
 
 	}
+
+
 
 
 
