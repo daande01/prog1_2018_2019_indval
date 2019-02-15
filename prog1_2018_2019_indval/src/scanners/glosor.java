3d9c8a8c189e3c4e37001d4425156429086ec59f
daande01 <daniel.evert.andersson@gmail.com>
2019-02-15 11:09:48 +0100

    ändringar i glosor

--- prog1_2018_2019_indval/src/scanners/glosor.java
+++ prog1_2018_2019_indval/src/scanners/glosor.java
@@ -8,42 +8,80 @@ public class glosor {
 	static LinkedList<String> sv = new LinkedList<String>();
 	static LinkedList<String> eng = new LinkedList<String>();
 
-
-
 	public static void main(String[] args) {
 
+		sv.add("katt");
+		eng.add("cat");
+		sv.add("bil");
+		eng.add("car");
 
 		Scanner s = new Scanner(System.in);
 
-		System.out.println("1 nyglosa \n 2 test \n 3 visa lista");
+		boolean fortgå = true;
 
-		int val = s.nextInt();
+		while (fortgå) {
+			System.out.println("1 nyglosa \n 2 test \n 3 visa lista \n  4 avsluta");
 
-		switch (val) {
-		case 1:
-			nyglosa();
+			int val = s.nextInt();
 
-			break;
-		case 2:
-			// test();
-			break;
+			switch (val) {
+			case 1:
+				nyglosa();
 
-		default:
+				break;
+			case 2:
+				// test();
+				break;
+			case 3:
 
-			break;
+				visaLista();
+				break;
+			case 4:
+
+				fortgå = false;
+				break;
+			default:
+
+				break;
+			}
 		}
-
 	}
 
 	public static void nyglosa() {
 
-		sv.add("katt");
+		Scanner sc = new Scanner(System.in);
 
+		System.out.println("ange en sv glosa");
+		String svord = sc.nextLine();
+		sv.add(svord);
+
+		System.out.println("ange motsvarande eng glosa");
+		String engord = sc.nextLine();
+		eng.add(engord);
 
 	}
 
+	public static void visaLista() {
 
+		for (int i = 0; i < sv.size(); i++) {
 
+			System.out.println(sv.get(i) + " == " + eng.get(i)+"\n");
 
+		}
+
+	}
+
+	public static void test() {
+
+		// looop räknare I=0
+
+		// visa ord på sv sv.get(i);
+		// läsa in eng ordet från användaren
+		// jämföra inmatatord med eng ord från lista
+		// ge poäng
+
+		// loop
+
+	}
 
 }
