395090cb929322ee2b37758957051e1af36d148e
daande01 <daniel.evert.andersson@gmail.com>
2019-05-17 10:15:38 +0200

    trafik ljus

--- prog1_2018_2019_indval/src/ritagrafik/Trafik.java
+++ prog1_2018_2019_indval/src/ritagrafik/Trafik.java
@@ -9,15 +9,45 @@ import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class Trafik extends JComponent{
 
-public  Trafik() {
-	this.setPreferredSize(new Dimension(200, 200));
+
+	private Color färg1=Color.GREEN;
+	private Color färg2=Color.black;
+	boolean state=true;
+	private Timer t1;
+
+
+public void stopTimer() {
+
+	t1.stop();
 }
 
+public  Trafik() {
+	this.setPreferredSize(new Dimension(200, 200));
+	 t1= new Timer(1000, e->  {
+
+		if (state==true) {
+
+			färg1=Color.BLACK;
+			färg2=Color.red;
+			state=false;
+
+		} else  {
+
+			färg1=Color.green;
+			färg2=Color.black;
+			state=true;
+
+		}
 
 
+		repaint();
+	});
+	t1.start();
+}
 
 
 @Override
@@ -27,9 +57,9 @@ protected void paintComponent(Graphics g) {
 
 	g.setColor(Color.black);
 	g.fillRect(5, 5, 30, 85);
-	g.setColor(Color.RED);
+	g.setColor(färg1);
 	g.fillOval(10, 10, 20, 20);
-	g.setColor(Color.green);
+	g.setColor(färg2);
 	g.fillOval(10, 60, 20, 20);
 
 
@@ -48,6 +78,14 @@ protected void paintComponent(Graphics g) {
 		JFrame f = new JFrame("trafiklus");
 
 		JButton stop=new JButton("stop");
+
+
+		stop.addActionListener(e->{
+
+			t.stopTimer();
+
+		});
+
 
 		JPanel p=new JPanel();
 
