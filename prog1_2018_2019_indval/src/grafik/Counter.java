14f05988a7b79b674d3c4bed46e9d9f71b7d2036
daande01 <daniel.evert.andersson@gmail.com>
2020-01-30 14:28:55 +0100

    boll

--- prog1_2018_2019_indval/src/grafik/Counter.java
+++ prog1_2018_2019_indval/src/grafik/Counter.java
@@ -6,13 +6,12 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-
-public class Counter extends JFrame{
+public class Counter extends JFrame {
 
 	JLabel l1 = new JLabel("10");
-	JButton plus=new JButton("+");
-	JButton minus=new JButton("-");
-	int tal=10;
+	JButton plus = new JButton("+");
+	JButton minus = new JButton("-");
+	int tal = 10;
 
 	public Counter() {
 
@@ -22,15 +21,15 @@ public class Counter extends JFrame{
 		add(plus);
 		add(minus);
 
-		plus.addActionListener(e->{
+		plus.addActionListener(e -> {
 
 			tal++;
 
-			l1.setText(""+tal);
+			l1.setText("" + tal);
 
 		});
 
-		minus.addActionListener(e->{
+		minus.addActionListener(e -> {
 
 			tal--;
 			l1.setText("" + tal);
